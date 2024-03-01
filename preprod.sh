5613c34b0bfd3b2650a30ae6abfaa7d628c9d11b
Driss <drissboumlik.dev@gmail.com>
2024-03-01 15:18:53 +0100

    Create bash script to update the preprod

---
+++ preprod.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# cd /var/www/html/b2b
+cd /d/Code/Work/Free/b2b/b2b_src
+git reset --hard HEAD
+# git pull origin preprod
+git pull rlab master
+
+echo "Run composer install ?: (leave it empty if no)"
+read run_composer_install
+
+echo "Run npm install ?: (leave it empty if no)"
+read run_npm_install
+
+echo "Run npm run prod ?: (leave it empty if no)"
+read run_npm_run_prod
+
+echo "Run php artisan seed:data ?: (leave it empty if no)"
+read run_php_artisan_seed_data
+
+echo "Run composer dumpautoload ?: (leave it empty if no)"
+read run_composer_dumpautoload
+
+echo "Edit storage, database, web_logs_ws permissions ?: (leave it empty if no)"
+read run_chmod
+
+if [[ -n "$run_composer_install" ]]; then composer install; fi
+if [[ -n "$run_npm_install" ]]; then npm install; fi
+if [[ -n "$run_npm_run_prod" ]]; then npm run production; fi;
+if [[ -n "$run_php_artisan_seed_data" ]]; then php artisan seed:data; fi;
+if [[ -n "$run_composer_dumpautoload" ]]; then composer dumpautoload; fi;
+if [[ -n "$run_chmod" ]]; then chmod 777 -R storage database web_logs_ws; fi;
